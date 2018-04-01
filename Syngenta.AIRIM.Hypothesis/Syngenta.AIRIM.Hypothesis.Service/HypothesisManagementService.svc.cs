872aaf1746ac2dd30c7a9d5e8187a00eb6c67b0a
NonStopMan <Mohammed.Thabet@outlook.com>
2018-04-01 19:43:38 +0200

    AIRIM

---
+++ Syngenta.AIRIM.Hypothesis/Syngenta.AIRIM.Hypothesis.Service/HypothesisManagementService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Syngenta.AIRIM.Hypothesis.Data;
+
+namespace Syngenta.AIRIM.Hypothesis.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HypothesisManagementService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select HypothesisManagementService.svc or HypothesisManagementService.svc.cs at the Solution Explorer and start debugging.
+    public class HypothesisManagementService : IHypothesisManagementService
+    {
+        public List<string> GetProjectCodes()
+        {
+            var context = new HypothesisEntities();
+            return context.APR_PROJECT_HYPOTHESIS_K_GET_PROJECT_CODES_P().Select(c => c.PROJECT_CODE).ToList();
+        }
+
+        public APR_PROJECT_HYPOTHESIS_K_GET_PROJECT_DETAILS_P_Result GetProjectDetails(string code)
+        {
+            var context = new HypothesisEntities();
+            var x = context.APR_PROJECT_HYPOTHESIS_K_GET_PROJECT_DETAILS_P(code);//.FirstOrDefault();
+            return new APR_PROJECT_HYPOTHESIS_K_GET_PROJECT_DETAILS_P_Result();
+        }
+    }
+}
---
+++ Syngenta.AIRIM.Hypothesis/Syngenta.AIRIM.Hypothesis.Service/IHypothesisManagementService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using Syngenta.AIRIM.Hypothesis.Data;
+
+namespace Syngenta.AIRIM.Hypothesis.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IHypothesisManagementService" in both code and config file together.
+    [ServiceContract]
+    public interface IHypothesisManagementService
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetProjectCodes", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        List<string> GetProjectCodes();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetProjectDetails/{code}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        APR_PROJECT_HYPOTHESIS_K_GET_PROJECT_DETAILS_P_Result GetProjectDetails(string code);
+
+        //[OperationContract]
+        //CompositeType GetProjectDetails(string projectCode);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+
+}
